a8ebf2dd17c234bfc9e5cd206017c374e7878ca0
RohitGupta8 <therohitg@gmail.com>
2021-10-09 10:30:18 +0530

    ADD : Ability to create BST using these files

---
+++ src/main/java/com/bl/binarysearchtree/MyBinaryTree.java
@@ -0,0 +1,31 @@
+package com.bl.binarysearchtree;
+
+public class MyBinaryTree<K extends Comparable<K>> {
+    public MyBinaryNode<K> root;
+
+    public void add(K key) {
+        this.root = this.addRecursively(root, key);
+
+    }
+
+    private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
+        if (current == null)
+            return new MyBinaryNode<>(key);
+        int compareResult = key.compareTo(current.key);
+        if (compareResult == 0)
+            return current;
+        if (compareResult < 0)
+            current.left = addRecursively(current.left, key);
+        else
+            current.right = addRecursively(current.right, key);
+        return current;
+    }
+
+    public int getSize() {
+        return this.getSizeRecursive(root);
+    }
+
+    public int getSizeRecursive(MyBinaryNode<K> current) {
+        return current == null ? 0 : 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
+    }
+}
