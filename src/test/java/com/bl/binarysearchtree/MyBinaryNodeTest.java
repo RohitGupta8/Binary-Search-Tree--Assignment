65acdfdd411c2b1b756d734e0cf2cc1baaf63321
RohitGupta8 <therohitg@gmail.com>
2021-10-09 10:56:10 +0530

    ADD : Ability to perform Test of search 63 in binary tree

--- src/test/java/com/bl/binarysearchtree/MyBinaryNodeTest.java
+++ src/test/java/com/bl/binarysearchtree/MyBinaryNodeTest.java
@@ -36,4 +36,26 @@ public class MyBinaryNodeTest {
         int size = myBinaryTree.getSize();
         Assert.assertEquals(13, size);
     }
+    @Test
+    public void givenNumbersWhenSearchToBSTShouldReturn63() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(65);
+
+        myBinaryTree.add(16);
+        myBinaryTree.add(3);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        int size = myBinaryTree.getSize();
+        boolean result = myBinaryTree.containsNodeRecursive(myBinaryTree.root, 63);
+        Assert.assertTrue(result);
+    }
 }
