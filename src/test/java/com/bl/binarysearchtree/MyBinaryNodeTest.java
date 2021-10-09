def7423e72d8d67ddc0cbb7e60aa0140df88e15f
RohitGupta8 <therohitg@gmail.com>
2021-10-09 10:46:24 +0530

    ADD : Ability to perform Test of create Binary tree shown in figure

--- src/test/java/com/bl/binarysearchtree/MyBinaryNodeTest.java
+++ src/test/java/com/bl/binarysearchtree/MyBinaryNodeTest.java
@@ -14,4 +14,26 @@ public class MyBinaryNodeTest {
         int size = myBinaryTree.getSize();
         Assert.assertEquals(3, size);
     }
+
+    @Test
+    public void givenNumbersWhenAddedToBSTShouldReturn13() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(65);
+
+        myBinaryTree.add(16);
+        myBinaryTree.add(3);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        int size = myBinaryTree.getSize();
+        Assert.assertEquals(13, size);
+    }
 }
